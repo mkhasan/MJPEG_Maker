66c5aac90ae101ecb00f57e5c800e8e3340ea1a9
mkhasan <hasan@dsmecasys.com>
2023-07-07 05:54:20 +0000

    Working

---
+++ build/CMakeFiles/mjpeg_maker.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/MyProjects/MJPEG_Maker/src/CRtspSession.cpp" "/home/ubuntu/MyProjects/MJPEG_Maker/build/CMakeFiles/mjpeg_maker.dir/src/CRtspSession.cpp.o"
+  "/home/ubuntu/MyProjects/MJPEG_Maker/src/CStreamer.cpp" "/home/ubuntu/MyProjects/MJPEG_Maker/build/CMakeFiles/mjpeg_maker.dir/src/CStreamer.cpp.o"
+  "/home/ubuntu/MyProjects/MJPEG_Maker/src/JPEGSamples.cpp" "/home/ubuntu/MyProjects/MJPEG_Maker/build/CMakeFiles/mjpeg_maker.dir/src/JPEGSamples.cpp.o"
+  "/home/ubuntu/MyProjects/MJPEG_Maker/src/client_handler.cpp" "/home/ubuntu/MyProjects/MJPEG_Maker/build/CMakeFiles/mjpeg_maker.dir/src/client_handler.cpp.o"
+  "/home/ubuntu/MyProjects/MJPEG_Maker/src/file_source.cpp" "/home/ubuntu/MyProjects/MJPEG_Maker/build/CMakeFiles/mjpeg_maker.dir/src/file_source.cpp.o"
+  "/home/ubuntu/MyProjects/MJPEG_Maker/src/image_writer.cpp" "/home/ubuntu/MyProjects/MJPEG_Maker/build/CMakeFiles/mjpeg_maker.dir/src/image_writer.cpp.o"
+  "/home/ubuntu/MyProjects/MJPEG_Maker/src/jpeg_writer.cpp" "/home/ubuntu/MyProjects/MJPEG_Maker/build/CMakeFiles/mjpeg_maker.dir/src/jpeg_writer.cpp.o"
+  "/home/ubuntu/MyProjects/MJPEG_Maker/src/mjpeg_maker.cpp" "/home/ubuntu/MyProjects/MJPEG_Maker/build/CMakeFiles/mjpeg_maker.dir/src/mjpeg_maker.cpp.o"
+  "/home/ubuntu/MyProjects/MJPEG_Maker/src/stream_source.cpp" "/home/ubuntu/MyProjects/MJPEG_Maker/build/CMakeFiles/mjpeg_maker.dir/src/stream_source.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DATA_DIR=\"\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/include"
+  "/home/mecasys/extension/ffmpeg_ffserver/ffmpeg"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mjpeg_maker.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "../bin/mjpeg_maker"
+  "../bin/mjpeg_maker.pdb"
+  "CMakeFiles/mjpeg_maker.dir/src/CRtspSession.cpp.o"
+  "CMakeFiles/mjpeg_maker.dir/src/CStreamer.cpp.o"
+  "CMakeFiles/mjpeg_maker.dir/src/JPEGSamples.cpp.o"
+  "CMakeFiles/mjpeg_maker.dir/src/client_handler.cpp.o"
+  "CMakeFiles/mjpeg_maker.dir/src/file_source.cpp.o"
+  "CMakeFiles/mjpeg_maker.dir/src/image_writer.cpp.o"
+  "CMakeFiles/mjpeg_maker.dir/src/jpeg_writer.cpp.o"
+  "CMakeFiles/mjpeg_maker.dir/src/mjpeg_maker.cpp.o"
+  "CMakeFiles/mjpeg_maker.dir/src/stream_source.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mjpeg_maker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
